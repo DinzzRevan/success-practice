f864005dcaece9588d1fc92cb1c85ff1eb1f5c39
DinzzRevan <dineshgokulan1994@gmail.com>
2021-06-21 14:15:39 +0530

    change task

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <v-container>
         <v-row>
             <v-col xs12>
-                <h3 class="text-h5 mt-1">Home Run! Git Hub Change</h3>
+                <h3 class="text-h5 mt-1">My First Change Git Hub Change</h3>
                 <v-divider class="grey lighten-3 mt-4"></v-divider>
             </v-col>
         </v-row>
